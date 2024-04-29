7488be164c639e2c14cdcc3e22d4281fb21bb772
maipato10 <monyekemaipato6@gmail.com>
2024-04-29 11:39:20 +0200

    scaffold basic ui with mock data

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #T3 Gallery
 
 ##TODO
--[] Make it deploy
--[] Tidy up build
--[] scaffold basic UI with mock data
+-[x] Make it deploy
+-[x] Tidy up build
+-[x] scaffold basic UI with mock data
 -[] Actually set up a database
 -[] Attach database to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
+import { db } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
 
+const mockUrls = [
+  "https://utfs.io/f/25c0c126-4ee1-4287-a370-d27360fde6aa-ibcluw.jpg",
+  "https://utfs.io/f/e4968c89-3387-40c4-9a44-c4bc3ef69bdb-ibclwm.jpg",
+  "https://utfs.io/f/573980cd-6de4-4ab6-9e1e-d4b0c1058040-nm33wo.png",
+  "https://utfs.io/f/9a0896fc-4180-4de5-9cd8-c7aacda3e476-ibclvr.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress; deployment successful2
+    <main className="">
+      <div className="flex flex-wrap">
+        {[...mockImages,...mockImages,...mockImages].map((image) => (
+          <div key={image.id} className="w-48 p-4">
+            <img src={image.url} alt={"image"}/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
