30d5138bf67e72ae46de8e0471edc7e88c60ca7a
maipato10 <monyekemaipato6@gmail.com>
2024-04-30 07:34:53 +0200

    sign in functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,35 @@ import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 //NB ALWAYS FROM HERE AND NOT VERCEL
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-2">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt={"image"} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
       <div className="flex flex-wrap">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt={"image"} />
-            <div>{image.name}</div>
-          </div>
-        ))}
+        <SignedOut>
+          <div className="h-full w-full text-2xl">Please sign in above</div>
+        </SignedOut>
+        <SignedIn>
+          <Images/>
+        </SignedIn>
       </div>
     </main>
   );
